fa29f8cc5214e2d7c068e46a7eccbf0ec6eaf899
Daniel Montoya <danielstmontoya@gmail.com>
2022-10-09 19:58:22 -0500

    main program finished

--- city.py
+++ city.py
@@ -2,15 +2,17 @@ import json
 from urllib.request import urlopen
 
 class City:
-  def __init__(self, name):
+  def __init__(self, name, country, key):
     self.name = name
+    self.country = country
+    self.key = key
     self.datos = {}
     self.temperatura = float
     self.humedad = float 
     self.wind = int
 
   def geo_code(self):
-    geo_code_url = f'http://api.openweathermap.org/geo/1.0/direct?q={self.name}&lang=en&appid=c8ea2e391834030a5f5c4ff79f169e6d'
+    geo_code_url = f'http://api.openweathermap.org/geo/1.0/direct?q={self.name},{self.country}&lang=en&appid={self.key}'
 
     self.datos['url'] = geo_code_url
 
@@ -23,19 +25,34 @@ class City:
 
   def info_weather(self):
     lat = self.datos['json']['lat']
-    print(f"""
-    =================
-    {lat}
-    ================
-    """)
     lon = self.datos['json']['lon']
-    print(f"""
-    =================
-    {lon}
-    ================
-    """)
-    weather_url = f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m,relativehumidity_2m,windspeed_10m'
-    weather_api = urlopen(weather_url)
-    decoded_json = json.loads(weather_api.read())
-    self.datos['clima'] = decoded_json
-
+    try:
+      weather_url = f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m,relativehumidity_2m,windspeed_10m'
+      weather_api = urlopen(weather_url)
+    except:
+      return "not found"
+    else:
+      decoded_json = json.loads(weather_api.read())
+      self.datos['clima'] = decoded_json
+  
+  def set_temperatura(self):
+    self.geo_code()
+    self.decode_url()
+    if self.info_weather() == "not found":
+      return "---"
+    self.info_weather()
+    self.temperatura = self.datos['clima']['hourly']['temperature_2m'][0]
+  def set_humidity(self):
+    self.geo_code()
+    self.decode_url()
+    if self.info_weather() == "not found":
+      return "---"
+    self.info_weather()
+    self.humedad = self.datos['clima']['hourly']['relativehumidity_2m'][0]
+  def set_wind(self):
+    self.geo_code()
+    self.decode_url()
+    if self.info_weather() == "not found":
+      return "---"
+    self.info_weather()
+    self.wind = self.datos['clima']['hourly']['windspeed_10m'][0]
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import json
 from city import City
 if __name__ == "__main__":
-  med = City('Bogota')
-  med.decode_url()
-  # print(med.datos['json'])
-  med.info_weather()
-  # print(med.datos['clima'])
-  print(json.dumps(med.datos, sort_keys=True, indent=4))
+  print("Welcome to the Weather CLI")
+  key = input("Enter your OpenWeather key:\n\t")
+  # info = City("bogota", "colombia", "c8ea2e391834030a5f5c4ff79f169e6d")
+  # info.set_temperatura()
+  # info.set_humidity()
+  # info.set_wind()
+  # print(info.datos)
+  while True:
+    city = input("Enter the city:\n\t").lower()
+    country = input("Enter the country:\n\t").lower()
+    info = City(city, country, key)
+    info.set_temperatura()
+    info.set_humidity()
+    info.set_wind()
+    print(f"""\n\t{city.capitalize()}, {country.capitalize()} - {info.temperatura}°C, humidy of {info.humedad}% and wind speed of {info.wind}km/h\n""")
+    quit = input("Try another city?\n\t")
+    if quit == "yes":
+      continue 
+    else:
+      break
